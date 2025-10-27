eb7d310360f9eaf3b0455e8d1a63216055a124c3
KariemMohammed <karemohamed254452@gmail.com>
2025-10-27 23:19:07 +0300

    Fix bug

--- F-React/src/App.jsx
+++ F-React/src/App.jsx
@@ -17,7 +17,7 @@ import FramWorkApp from './Components/FramWorkApp/FramWorkApp'
 
 let x = createBrowserRouter([
   {
-    path: '/FramWorkApp/',
+    path: '/F-React/',
     element: <Layout />,
     children: [
       { index: true, element: <FramWorkApp /> },
